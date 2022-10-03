94ea48ee12c25dfd07916c435cf61aad9c7a7eb9
Vasil Sokolov <vasil@afterlogic.com>
2022-10-03 11:21:28 +0200

    renamed methods in Vuex storage

--- vue-mobile/store/actions.js
+++ vue-mobile/store/actions.js
@@ -1,9 +1,7 @@
 import types from 'src/utils/types'
 import contactsWebApi from '../contacts-web-api'
 
-import {
-  getParseContacts,
-} from '../utils/common'
+import { getParseContacts } from '../utils/common'
 
 export default {
   asyncGetStorages: async ({ commit }) => {
@@ -12,16 +10,18 @@ export default {
       if (storages.length > 2) {
         storages.unshift({Id: 'all'})
       }
-      commit('SET_STORAGE_LIST', storages)
-      commit('SET_CURRENT_STORAGE', storages.length ? storages[0] : {})
+      commit('setStorageList', storages)
+      commit('setCurrentStorage', storages.length ? storages[0] : {})
     }
   },
+
   asyncGetGroups: async ({ commit }) => {
     const groups = await contactsWebApi.getGroups()
     if (types.pArray(groups)) {
-      commit('SET_GROUPS_LIST', groups)
+      commit('setGroupList', groups)
     }
   },
+
   asyncGetContacts: async ({ commit, getters, dispatch }) => {
     dispatch('changeLoadingStatus', true)
     const currentStorage = getters['currentStorage']
@@ -35,78 +35,94 @@ export default {
     const data = await contactsWebApi.getContacts(parameters)
     if (types.pArray(data?.List)) {
       const contacts = getParseContacts(data.List)
-      // const contacts = data.List
-      console.log('contacts', contacts)
-      commit('SET_CONTACTS_LIST', contacts)
+      commit('setContactsList', contacts)
     }
     dispatch('changeLoadingStatus', false)
   },
+
   asyncGetContact: async ({ commit, dispatch }, parameters) => {
     dispatch('changeLoadingStatus', true)
     const data = await contactsWebApi.getContact(parameters)
-    commit('SET_CURRENT_CONTACT', data)
+    commit('setCurrentContact', data)
     dispatch('changeLoadingStatus', false)
   },
+
   asyncGetContactsSuggestions: async ({}, parameters) => {
     return await contactsWebApi.getContactSuggestions(parameters)
   },
+
   asyncCreateContact: async ({ getters, dispatch }) => {
     const newContact = getters['newContact']
 
     await contactsWebApi.createContact({ Contact: newContact })
     dispatch('asyncGetContacts')
   },
+
   asyncCreateGroup: async ({ getters, dispatch }) => {
     const newGroup = getters['newGroup']
 
     await contactsWebApi.createGroup({ Group: newGroup })
     dispatch('asyncGetGroups')
   },
+
   changeCurrentStorage: ({ commit }, storage) => {
-    commit('SET_CURRENT_GROUP', '')
-    commit('SET_CURRENT_STORAGE', storage)
+    commit('setCurrentGroup', '')
+    commit('setCurrentStorage', storage)
   },
+
   changeCurrentGroup: ({ commit }, group) => {
-    commit('SET_CURRENT_STORAGE', {})
-    commit('SET_CURRENT_GROUP', group)
+    commit('setCurrentStorage', {})
+    commit('setCurrentGroup', group)
   },
+
   changeLoadingStatus: ({ commit }, status) => {
-    commit('SET_LOADING_STATUS', status)
+    commit('setLoadingStatus', status)
   },
+
   changeCurrentHeader: ({ commit }, headerName) => {
-    commit('SET_CURRENT_HEADER_NAME', headerName)
+    commit('setCurrentHeaderName', headerName)
   },
+
   changeSearchText: ({ commit }, text) => {
-    commit('SET_SEARCH_TEXT', text)
+    commit('setSearchText', text)
   },
+
   changeNewContact: ({ commit }, contact) => {
-    commit('SET_NEW_CONTACT', contact)
+    commit('setNewContact', contact)
   },
+
   changeNewGroup: ({ commit }, group) => {
-    commit('SET_NEW_GROUP', group)
+    commit('setNewGroup', group)
   },
+
   changeDialogComponent: ({ commit }, dialogComponent) => {
     commit('setDialogComponent', dialogComponent)
   },
-  asyncDeleteContact: async ({}, params) => {
-    return await contactsWebApi.deleteContact(params)
+
+  asyncDeleteContacts: async ({}, params) => {
+    return await contactsWebApi.deleteContacts(params)
   },
-  removeContact: ({ commit }, contact) => {
-    commit('removeContact', contact)
-  },
+
+  // removeContact: ({ commit }, contact) => {
+  //   commit('removeContact', contact)
+  // },
+
   asyncShareContact: async ({}, params) => {
     return await contactsWebApi.shareContact(params)
   },
+
   asyncUnShareContact: async ({}, params) => {
     return await contactsWebApi.shareContact(params)
   },
+
   asyncEditContact: async ({}, params) => {
     return await contactsWebApi.updateContact(params)
   },
+
   changeSelectStatus: ({ commit }, contact) => {
-    console.log('changeSelectStatus action', contact)
     commit('setSelectStatus', contact)
   },
+
   resetSelectedItems: ({ commit }, { items }) => {
     commit('resetSelectedItems', items)
   },
