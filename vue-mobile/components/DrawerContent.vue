32f0716a92ce5a4440034d0ed3c9e4b77b356682
Christina <c.parevskaya@afterlogic.com>
2022-10-03 12:46:55 +0300

    add separators

--- vue-mobile/components/DrawerContent.vue
+++ vue-mobile/components/DrawerContent.vue
@@ -2,6 +2,7 @@
   <div class="contacts__drawer-head q-pl-lg" style="padding-top: 32px">
     {{$t('CONTACTSMOBILEWEBCLIENT.LABEL_STORAGES')}}
   </div>
+  <q-separator spaced class="separator-color"/>
 
   <div>
     <storage-item
@@ -11,6 +12,8 @@
         :storage="storage"
     />
   </div>
+
+  <q-separator spaced class="separator-color"/>
 
   <div class="flex items-center justify-between q-pa-md q-pl-lg">
     <div class="text-grey">
@@ -73,5 +76,8 @@ export default {
   font-size: 14px;
   line-height: 25px;
   color: #969494;
+}
+.separator-color {
+  background: #F6F6F6;
 }
 </style>
